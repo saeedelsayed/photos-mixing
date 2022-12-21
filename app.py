8a9b7bbfe3a7ee6aa3772d25fcbd5adf9cffbfe1
diaabadr <diaabadr82@gmail.com>
2022-12-21 03:00:18 +0200

    javascript file almost finished

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 skills_app = Flask(__name__)
 
@@ -7,9 +8,17 @@ skills_app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# route for testing axios
 
+
+@skills_app.route('/merge', methods=['POST'])
+def fun():
+    # print('diaa')
+    # print(request.form['formData'])
+    # imageFile = request.files.get('file','')
+    # print(request.files)
+    return (request.files)
 
 
 if __name__ == "__main__":
     skills_app.run()
-
