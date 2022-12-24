52e8b1d46ff50e4a794eba14c2b2bd8ea171b9de
diaabadr <diaabadr82@gmail.com>
2022-12-24 21:34:44 +0200

    app.py modification test

--- app.py
+++ app.py
@@ -19,7 +19,13 @@ def fun():
     sImage=request.values['sImage']
     sImageCropped=request.values['sImageCropped']
     print(fImage,fImageCropped,sImage,sImageCropped)
-    
+    from PIL import Image
+    import glob
+    image_list = []
+    for filename in glob.glob('static/image.png'): #assuming gif
+        im=Image.open(filename)
+        image_list.append(im)
+    print(image_list)    
     return ("generated photo name")
 
 
