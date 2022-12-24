6723f4f7bf253bf4a8218ff3d5657a55fb62db84
Maryam Megahed <maryam.megahed45@gmail.com>
2022-12-24 21:32:21 +0200

    save error

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
+
 from flask import Flask, render_template, request
-
-
+import os;
+from PIL import Image
+import glob
+from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import functions as fn
 skills_app = Flask(__name__)
 
+def images(imageName):
+    image_list = []
+    for filename in glob.glob("upLoads/"+imageName):
+      im=Image.open(filename)
+      image_list.append(im)
+      #print(filename)
+      print(image_list);
+      return image_list[0];
+      im = Image.fromarray(arrayfrommerge)
+      im.save('test.png')
+      matplotlib.pyplot.imsave(fname, arr)
 
+     
+    
+
+    
 @skills_app.route("/")
 def home():
     return render_template("index.html")
@@ -19,9 +39,11 @@ def fun():
     sImage=request.values['sImage']
     sImageCropped=request.values['sImageCropped']
     print(fImage,fImageCropped,sImage,sImageCropped)
-    
-    return ("generated photo name")
-
+    compined_image=fn.merge("upLoads/"+fImage,"upLoads/"+fImageCropped,"upLoads/"+sImage,"upLoads/"+sImageCropped)
+    plt.imsave("upLoads/result", compined_image)
+    # im = Image.fromarray(compined_image)
+    # im.save('result.png')
+    return ("result")
 
 if __name__ == "__main__":
     skills_app.run(port=5000)
